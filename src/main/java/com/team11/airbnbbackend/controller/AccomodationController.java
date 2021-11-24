a4c0efb3012b513354c2287c45f9d4bf91a760a7
namusik <wsnam0418@gmail.com>
2021-11-25 00:34:07 +0900

    travis test

--- src/main/java/com/team11/airbnbbackend/controller/AccomodationController.java
+++ src/main/java/com/team11/airbnbbackend/controller/AccomodationController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+//테스트
 @RestController
 public class AccomodationController {
 
